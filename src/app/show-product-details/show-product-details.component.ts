c732ee5c80f0e1a5f56f9508cfc62da8e8490083
Fiiider12345 <marek.pecho0@gmail.com>
2023-05-22 16:40:28 +0200

    Show Images - Image Bytes to File

--- src/app/show-product-details/show-product-details.component.ts
+++ src/app/show-product-details/show-product-details.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ProductService } from '../_services/product.service';
 import { Product } from '../_model/product.model';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ShowProductImagesDialogComponent } from '../show-product-images-dialog/show-product-images-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageProcessingService } from '../image-processing.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-show-product-details',
@@ -11,18 +15,23 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class ShowProductDetailsComponent {
 
   productDetails: Product[] = [];
-  displayedColumns: string[] = ['Id', 'Product Name', 'Product Description', 'Product Discounted Price', 'Product Actual Price', 'Edit', 'Delete'];
+  displayedColumns: string[] = ['Id', 'Product Name', 'Product Description', 'Product Discounted Price', 'Product Actual Price', 'Images', 'Edit', 'Delete'];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService,
+    public imagesDialog: MatDialog,
+    private imageProcessingService: ImageProcessingService) {}
 
   ngOnInit(): void {
     this.getAllProducts();
   }
 
   public getAllProducts() {
-    this.productService.getAllProducts().subscribe(
+    this.productService.getAllProducts()
+    .pipe(
+      map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
+    )
+    .subscribe(
       (resp: Product[]) => {
-        console.log(resp);
         this.productDetails = resp;
       }, (error: HttpErrorResponse) => {
         console.log(error);
@@ -40,5 +49,16 @@ export class ShowProductDetailsComponent {
         console.log(error);
       }
     );
+  }
+
+  showImages(product: Product) {
+    console.log(product);
+    this.imagesDialog.open(ShowProductImagesDialogComponent,{
+      data: {
+        images: product.productImages
+      },
+      height: '500px',
+      width: '800px'
+    });
   }
 }
